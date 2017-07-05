be35e448fdf25a21a143cf87782fbc1165483f65
ksauby <ksauby@gmail.com>
2017-07-05 11:51:15 -0400

    working on issue #43 in ksauby/GTMNERR_analysis

--- R/estimateCactusDensityperPlot.R
+++ R/estimateCactusDensityperPlot.R
@@ -14,7 +14,7 @@ estimateCactusDensityperPlot <- function(Plant_Surveys_by_Year, Plot_Info) {
 		summarise(n_plots = length(Tag_Number))
 	# count plants per demographic plot per demographic survey
 	temp_A <- Plant_Surveys_by_Year %>%
-		filter(DeadbyEndofYear==0 & MissingbyEndofYear==0) %>%
+		# filter(DeadbyEndofYear==0 & MissingbyEndofYear==0) %>%
 		group_by(FecundityYear, Network, Species) %>%
 		summarise(
 			#N_plants = length(unique(PlantID)),
@@ -36,12 +36,35 @@ estimateCactusDensityperPlot <- function(Plant_Surveys_by_Year, Plot_Info) {
 	temp_A %<>%
 		rowwise() %>%
 		mutate(
-			cactus_density = pusilla_density + stricta_density,
-			pusilla_density_per_plot = pusilla_density/n_plots,
-			stricta_density_per_plot = stricta_density/n_plots,
-			cactus_density_per_plot = cactus_density/n_plots
+			cactus_density = pusilla_density + stricta_density #,
+			# pusilla_density_per_plot = pusilla_density/n_plots,
+			# stricta_density_per_plot = stricta_density/n_plots,
+			# cactus_density_per_plot = cactus_density/n_plots
 		)
-	Plant_Surveys_by_Year %<>% 
-		merge(temp_A, by=c("FecundityYear", "Network", "Island"))
+	temp_B <- Plant_Surveys_by_Year %>% 
+		merge(temp_A, by=c("FecundityYear", "Network", "Island")) %>%
+	# subtract focal plant's biomass
+		rowwise %>%
+		mutate(
+			cactus_density_per_plot = 
+				sum(cactus_density,-(Size_t), na.rm=T)/n_plots,
+			pusilla_density_per_plot = NA,
+			stricta_density_per_plot = NA,
+			pusilla_density_per_plot = replace(
+				pusilla_density_per_plot,
+				which(Species=="Opuntia pusilla"),
+				sum(pusilla_density,-(Size_t), na.rm=T)/n_plots
+			),
+			stricta_density_per_plot = replace(
+				stricta_density_per_plot,
+				which(Species=="Opuntia stricta"),
+				sum(stricta_density,-(Size_t), na.rm=T)/n_plots
+			)
+		) %>%
+		as.data.frame
+		
+		
+
+		)
 	return(Plant_Surveys_by_Year)
 }
