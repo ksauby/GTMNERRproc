f14660aeb271efe0aa53dc4dd6b00d06fce271f5
ksauby <ksauby@gmail.com>
2017-07-09 17:18:30 -0400

    close ksauby/GTMNERR_analysis#43

--- R/estimateCactusDensityperPlot.R
+++ R/estimateCactusDensityperPlot.R
@@ -7,21 +7,18 @@
 #'
 #' @export
 
-estimateCactusDensityperPlot <- function(Plant_Surveys_by_Year, Plot_Info) {
+estimateCactusDensityperPlot <- function(Plant.Surveys.by.Year, Plot.Info) {
 	# need to control for # of plots in network
-	temp <- Plot_Info %>%
+	temp <- Plot.Info %>%
 		group_by(Network) %>%
 		summarise(n_plots = length(Tag_Number))
 	# count plants per demographic plot per demographic survey
-	temp_A <- Plant_Surveys_by_Year %>%
-		# filter(DeadbyEndofYear==0 & MissingbyEndofYear==0) %>%
+	temp_A <- Plant.Surveys.by.Year %>%
 		# remove records of plants outside of the plot
 		filter(OutsideOfPlot!="Yes") %>%
-		group_by(FecundityYear, Network, Species) %>%
+		group_by(Island, FecundityYear, Network, Species) %>%
 		summarise(
-			#N_plants = length(unique(PlantID)),
-			N_segments = sum(Size_t, na.rm=T),
-			Island = Island[1]
+			N_segments = sum(Size_t, na.rm=T)
 		) %>%
 		as.data.frame %>%
 		arrange(Network, FecundityYear) %>%
@@ -38,12 +35,9 @@ estimateCactusDensityperPlot <- function(Plant_Surveys_by_Year, Plot_Info) {
 	temp_A %<>%
 		rowwise() %>%
 		mutate(
-			cactus_density = pusilla_density + stricta_density #,
-			# pusilla_density_per_plot = pusilla_density/n_plots,
-			# stricta_density_per_plot = stricta_density/n_plots,
-			# cactus_density_per_plot = cactus_density/n_plots
+			cactus_density = pusilla_density + stricta_density
 		)
-	temp_B <- Plant_Surveys_by_Year %>% 
+	temp_B <- Plant.Surveys.by.Year %>% 
 		merge(temp_A, by=c("FecundityYear", "Network", "Island")) %>%
 	# subtract focal plant's biomass
 		rowwise %>%
