5d3cec87798cccc307e44891fbdce972fe6537fe
ksauby <ksauby@gmail.com>
2017-09-29 16:03:44 -0400

    added to set of functions

---
+++ R/calculateStageDistribution.R
@@ -0,0 +1,19 @@
+#' Calculate Observed Stage Distribution
+#'
+#' @param dataset
+#'
+#' @export
+
+calculateStageDistribution <- function(dataset) {
+	dataset %>% map(
+		~ .$trans01 %>% 
+			group_by(stage) %>%
+			summarise(count = n()) %>%
+			mutate(
+				Stable Stage Value = count/sum(count),s
+				SeedBankSize = "Observed", 
+				SeedsPerFruit = "Observed", 
+				SeedSurvival = "Observed"
+			)
+	)
+}
